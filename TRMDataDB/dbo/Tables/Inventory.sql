049e7f3522d167b817b8615a47ee49016d2b54ad
Степан Макаров <89526203512bvk@gmail.com>
2023-04-25 16:05:57 +0300

    Add DB and MVC
    Register DB Tables for the basic register system
    and Add Empty MVC project

---
+++ TRMDataDB/dbo/Tables/Inventory.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Inventory]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [ProductId] INT NOT NULL, 
+    [Quantity] INT NOT NULL, 
+    [PurchasePrce] MONEY NOT NULL, 
+    [PurchaseDate] DATETIME2 NOT NULL DEFAULT getutcdate(), 
+    CONSTRAINT [FK_Inventory_Products] FOREIGN KEY (ProductId) REFERENCES Products(Id)
+)
---
+++ TRMDataDB/dbo/Tables/SalesDetails.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[SalesDetails]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY,
+    [SaleId] INT NOT NULL, 
+    [ProductId] INT NOT NULL, 
+    [Quantity] INT NOT NULL DEFAULT 1, 
+    [PurchasePrice] MONEY NOT NULL, 
+    [Tax] MONEY NOT NULL DEFAULT 0, 
+    CONSTRAINT [FK_SalesDetails_Sales] FOREIGN KEY (SaleId) REFERENCES  Sales(Id), 
+    CONSTRAINT [FK_SalesDetails_Products] FOREIGN KEY (ProductId) REFERENCES Products(Id)
+)
