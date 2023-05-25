51ce35e47f9d34e09f69e28a4122343113b2c178
Степан Макаров <89526203512bvk@gmail.com>
2023-05-25 12:39:11 +0300

    Adding  error message for WPF Login form
    Adding error message for WPF Login form if
    authorization was rejected

--- TRMDesktopUIwpf/ViewModels/LoginViewModel.cs
+++ TRMDesktopUIwpf/ViewModels/LoginViewModel.cs
@@ -41,6 +41,35 @@ namespace TRMDesktopUIwpf.ViewModels
             }
         }
 
+        public bool IsErrorMessageVisible
+        {
+            get 
+            {
+                bool output = false;
+
+                if (ErrorMessage?.Length > 0)
+                {
+                    output = true;
+                } 
+
+                return output;
+            }
+        }
+
+        private string? _errorMessage;
+
+        public string? ErrorMessage
+        {
+            get { return _errorMessage; }
+            set
+            {
+                _errorMessage = value;
+                NotifyOfPropertyChange(() => ErrorMessage);
+                NotifyOfPropertyChange(() => IsErrorMessageVisible);
+            }
+        }
+
+
         public bool CanLogIn
         {
             get
@@ -60,12 +89,13 @@ namespace TRMDesktopUIwpf.ViewModels
         {
             try
             {
+                ErrorMessage = "";
                 var result = await _apiHelper.Authenticate(UserName, Password);
             }
             catch (Exception ex)
             {
 
-                Console.WriteLine(ex.Message);
+                ErrorMessage = ex.Message;
             }
             
         }
