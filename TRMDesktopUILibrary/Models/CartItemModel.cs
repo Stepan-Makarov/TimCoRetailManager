98c254835f012b548b8e5c5bb2953a308c9076b1
Степан Макаров <89526203512bvk@gmail.com>
2023-06-05 18:15:18 +0300

    Get AddToCart method working
    Adds the functionality to  add products ro the cart.
    get the SubTotal and update quantities in cart

---
+++ TRMDesktopUILibrary/Models/CartItemModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TRMDesktopUILibrary.Models
+{
+    public class CartItemModel
+    {
+        public UIProductModel? Product { get; set; }
+        public int QuantityInCart { get; set; }
+        public string? DisplayProductData
+        {
+            get 
+            { 
+                return $"{Product.ProductName} ({QuantityInCart})"; 
+            }
+        }
+
+    }
+}
--- TRMDesktopUIwpf/ViewModels/SalesViewModel.cs
+++ TRMDesktopUIwpf/ViewModels/SalesViewModel.cs
@@ -12,6 +12,7 @@ namespace TRMDesktopUIwpf.ViewModels
 {
     public class SalesViewModel : Screen
     {
+        private readonly IProductEndpoint _productEndpoint;
         public SalesViewModel(IProductEndpoint productEndpoint)
         {
             _productEndpoint = productEndpoint;
@@ -41,9 +42,22 @@ namespace TRMDesktopUIwpf.ViewModels
             }
 		}
 
-        private BindingList<UIProductModel>? _cart;
+        private UIProductModel _selectedProduct;
 
-        public BindingList<UIProductModel>? Cart
+        public UIProductModel SelectedProduct
+        {
+            get { return _selectedProduct; }
+            set 
+            {
+                _selectedProduct = value;
+                NotifyOfPropertyChange(() => SelectedProduct);
+                NotifyOfPropertyChange(() => CanAddToCart);
+            }
+        }
+
+        private BindingList<CartItemModel> _cart = new BindingList<CartItemModel>();
+
+        public BindingList<CartItemModel> Cart
         {
             get { return _cart; }
             set
@@ -53,25 +67,81 @@ namespace TRMDesktopUIwpf.ViewModels
             }
         }
 
-        private int _itemQuantity;
-        private readonly IProductEndpoint _productEndpoint;
-
-        public int ItemQuantity
+        private string? _itemQuantity = "1";
+        public string? ItemQuantity
         {
 			get { return _itemQuantity; }
 			set 
 			{
                 _itemQuantity = value;
                 NotifyOfPropertyChange(() => ItemQuantity);
+                NotifyOfPropertyChange(() => CanAddToCart);
+                NotifyOfPropertyChange(() => CanParseItemQuantityToInt);
             }
 		}
+       
+        private string? _errorMessage;
+
+        public string? ErrorMessage
+        {
+            get { return _errorMessage; }
+            set
+            {
+                _errorMessage = value;
+                NotifyOfPropertyChange(() => ErrorMessage);
+                NotifyOfPropertyChange(() => IsErrorMessageVisible);
+            }
+        }
+        public bool IsErrorMessageVisible
+        {
+            get
+            {
+                bool output = false;
+
+                if (CanParseItemQuantityToInt == false)
+                {
+                    output = true;
+                }
+
+                return output;
+            }
+        }
+        public int ItemQuantityToInt
+        {
+            get
+            {
+                int itemQuantity;
+                bool canParseToInt = int.TryParse(ItemQuantity, out itemQuantity);
+                return itemQuantity;
+            }
+        }
+
+        public bool CanParseItemQuantityToInt
+        {
+            get
+            {
+                bool canParseToInt = true;
+
+                if (ItemQuantity != null)
+                {
+                    canParseToInt = int.TryParse(ItemQuantity, out int itemQuantity);
+                }
+
+                return canParseToInt;
+            }
+        }
 
         public string? SubTotal
         {
             get 
             {
-                //Replace weth colculations
-                return "0.00 ₽";
+                decimal subTotal = 0;
+
+                foreach (var item in Cart)
+                {
+                    subTotal += (item.Product.RetailPrice * item.QuantityInCart);
+                }
+                return subTotal.ToString("C");
             }
         }
         public string? Tax
@@ -95,18 +165,52 @@ namespace TRMDesktopUIwpf.ViewModels
         {
             get
             {
+                ErrorMessage = "";
+                if (CanParseItemQuantityToInt == false)
+                {
+                    ErrorMessage = "Type a valid number";
+                }
+
                 bool output = false;
 
                 //Something is checked
                 //Something is in ItemQuantity
+                if (CanParseItemQuantityToInt && ItemQuantityToInt > 0 && SelectedProduct?.QuantityInStock >= ItemQuantityToInt)
+                {
+                    output = true;
+                }
 
                 return output;
             }
         }
-
+        
         public void AddToCart()
         {
-            //Add To Cart
+            CartItemModel existingItem = Cart.FirstOrDefault(x => x.Product == SelectedProduct);
+
+            if (existingItem != null)
+            {
+                existingItem.QuantityInCart += ItemQuantityToInt;
+
+                //There should be a better code
+                Cart.Remove(existingItem);
+                Cart.Add(existingItem);
+            }
+
+            else
+            {
+                CartItemModel? CartItem = new CartItemModel
+                {
+                    Product = SelectedProduct,
+                    QuantityInCart = ItemQuantityToInt
+                };
+                Cart.Add(CartItem);
+            }
+
+            SelectedProduct.QuantityInStock -= ItemQuantityToInt;
+            ItemQuantity = "1";
+
+            NotifyOfPropertyChange(() => SubTotal);
         }
 
         public bool CanRemoveFromCart
@@ -124,6 +228,7 @@ namespace TRMDesktopUIwpf.ViewModels
         public void CheckOut()
         {
             //Remove From Cart
+            NotifyOfPropertyChange(() => SubTotal);
         }
 
         public bool CanCheckOut
