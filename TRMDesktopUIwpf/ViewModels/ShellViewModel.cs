43a2e7f2da1112c8ef40acfb37070d323cac6af9
Степан Макаров <89526203512bvk@gmail.com>
2023-06-07 18:10:39 +0300

    Override OnStartup in App class in WPF
    Override OnStartup methode in WPF application
    for displaying currency not in dollar.
    Made fancier Product list

--- TRMDesktopUIwpf/ViewModels/ShellViewModel.cs
+++ TRMDesktopUIwpf/ViewModels/ShellViewModel.cs
@@ -12,16 +12,14 @@ namespace TRMDesktopUIwpf.ViewModels
     public class ShellViewModel : Conductor<object>, IHandle<LogOnEvent>
     {
         private readonly IEventAggregator _events;
-        private readonly SimpleContainer _container;
         private readonly SalesViewModel _salesVM;
 
-        public ShellViewModel(IEventAggregator events, SimpleContainer container, SalesViewModel salesVM)
+        public ShellViewModel(IEventAggregator events, SalesViewModel salesVM)
         {
-            _container = container;
             _events = events;
             _salesVM = salesVM;
             _events.SubscribeOnPublishedThread(this);
-            ActivateItemAsync(_container.GetInstance<LoginViewModel>());
+            ActivateItemAsync(IoC.Get<LoginViewModel>());
         }
 
         public async Task HandleAsync(LogOnEvent message, CancellationToken cancellationToken)
