439e11c57bd39484dcc7da47a8190849b42a5bf2
Степан Макаров <green_hill98@mail.ru>
2023-08-16 18:26:09 +0800

    Add Code For Creating Roles
    Add code in HomeController for creating roles
    and commenting it becouse it is needed only
    onсе.

--- TRMApi/Controllers/HomeController.cs
+++ TRMApi/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 using TRMApi.Models;
 
@@ -7,14 +8,40 @@ namespace TRMApi.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly UserManager<IdentityUser> _userManager;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger,
+                              RoleManager<IdentityRole> roleManager,
+                              UserManager<IdentityUser> userManager)
         {
             _logger = logger;
+            _roleManager = roleManager;
+            _userManager = userManager;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
+            //string[] roles = { "Admin", "Manager", "Cashier" };
+
+            //foreach (var role in roles)
+            //{
+            //    var roleExist = await _roleManager.RoleExistsAsync(role);
+
+            //    if (roleExist == false)
+            //    {
+            //        await _roleManager.CreateAsync(new IdentityRole(role));
+            //    }
+            //}
+
+            //var user = await _userManager.FindByEmailAsync("Tom@mail.ru");
+
+            //if (user != null)
+            //{
+            //    await _userManager.AddToRoleAsync(user, "Admin");
+            //    await _userManager.AddToRoleAsync(user, "Cashier");
+            //}
+
             return View();
         }
 
