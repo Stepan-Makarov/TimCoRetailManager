813bda3c3591d98506ce66fc1c20e0edc8c2e7a7
Степан Макаров <green_hill98@mail.ru>
2023-09-06 11:56:53 +0800

    Create AuthenticationServece Class
    Create class with Login and Logout methods

---
+++ Portal/Models/AuthenticationService.cs
@@ -0,0 +1,65 @@
+﻿using Blazored.LocalStorage;
+using System.Net.Http.Headers;
+using System.Security.Cryptography.X509Certificates;
+using System.Text.Json;
+
+namespace Portal.Models
+{
+    public class AuthenticationService : IAuthenticationService
+    {
+        private readonly HttpClient _client;
+        private readonly AuthStateProvider _authStateProvider;
+        private readonly ILocalStorageService _localStorage;
+
+        public AuthenticationService(HttpClient client,
+                                     AuthStateProvider authStateProvider,
+                                     ILocalStorageService localStorage)
+        {
+            _client = client;
+            _authStateProvider = authStateProvider;
+            _localStorage = localStorage;
+        }
+
+        public async Task<AuthenticatedUserModel> Login(AuthenticationUserModel userForAuthentication)
+        {
+            var data = new FormUrlEncodedContent(new[]
+            {
+                //new KeyValuePair<string, string>("grant_type", "password"),
+                new KeyValuePair<string, string>("username", userForAuthentication.Email),
+                new KeyValuePair<string, string>("password", userForAuthentication.Password)
+            });
+
+            var authResult = await _client.PostAsync("https://localhost:7296/Token", data);
+            //var content = await authResult.Content.ReadAsStringAsync();
+
+            if (authResult.IsSuccessStatusCode)
+            {
+                var authContent = await authResult.Content.ReadAsStringAsync();
+                var result = JsonSerializer.Deserialize<AuthenticatedUserModel>(
+                    authContent,
+                    new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+
+                await _localStorage.SetItemAsync("authToken", result.Access_Token);
+
+                _authStateProvider.NotifyUserAuthentication(result.Access_Token);
+
+                _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("bearer", result.Access_Token);
+
+                return result;
+            }
+
+            else
+            {
+                return null;
+                //throw new Exception(authResult.ReasonPhrase);
+            }
+        }
+
+        public async Task Logout()
+        {
+            await _localStorage.RemoveItemAsync("authToken");
+            _authStateProvider.NotifyUserLogout();
+            _client.DefaultRequestHeaders.Authorization = null;
+        }
+    }
+}
